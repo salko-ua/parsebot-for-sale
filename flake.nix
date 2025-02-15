cf66728a36973954fb910660dbbbfab64debae56
salko-ua <github@salko-ua.de>
2025-02-15 13:49:15 +0200

    switch to nix & .direnv from poetry

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (self) outputs;
+        inherit (pkgs) lib;
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+      in
+      {
+        inherit pkgs;
+
+        devShell = pkgs.mkShell {
+          packages =
+            with pkgs;
+            [
+              postgresql
+              python3
+              python3Packages.sqlalchemy
+              python3Packages.pytest
+              python3Packages.aiohttp
+              python3Packages.requests
+              python3Packages.aiosqlite
+            ] ++ python3Packages.fastapi.optional-dependencies.standard;
+          PYTHONPATH = ".";
+        };
+
+      }
+    );
+}
