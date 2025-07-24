7ad5aad8af72bfc8ae9a190231d7e3dfc62095c0
SALO <github@salko-ua.de>
2025-07-24 11:02:30 +0300

    Update __init__.py

--- src/olx_api/__init__.py
+++ src/olx_api/__init__.py
@@ -140,7 +140,7 @@ class Parser:
         checklist = []
 
         # find all span tags in div
-        div_with_tags = self.soup.find("div", class_="css-5l1a1j")
+        div_with_tags = self.soup.find("div", class_="css-1g2c38u")
         if div_with_tags and isinstance(div_with_tags, Tag):
             tags = div_with_tags.find_all("p")
         else:
